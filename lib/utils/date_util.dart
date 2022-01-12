83affdda09ca3b67153b086ab64b9c0311bfdf2d
quandm <quandm@imt-soft.com>
2022-01-12 21:07:24 +0700

    + implement date util

--- lib/utils/date_util.dart
+++ lib/utils/date_util.dart
@@ -20,4 +20,14 @@ class AppDateUtils {
     }
     return newList;
   }
+
+  static bool isSameDate(DateTime date1, DateTime date2) {
+    return date1.day == date2.day &&
+        date1.month == date2.month &&
+        date1.year == date2.year;
+  }
+
+  static bool isSameYearAndMonth(DateTime date1, DateTime date2) {
+    return date1.month == date2.month && date1.year == date2.year;
+  }
 }
